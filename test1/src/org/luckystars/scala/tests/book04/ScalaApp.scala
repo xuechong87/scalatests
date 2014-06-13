f42b0b573149dda771682c50fa00050f2074ec3d
xuechong <xuechong87@gmail.com>
2014-06-13 10:47:47 +0800

    add scala app

---
+++ test1/src/org/luckystars/scala/tests/book04/ScalaApp.scala
@@ -0,0 +1,5 @@
+package org.luckystars.scala.tests.book04
+
+object ScalaApp extends Application {
+	println("aaaa")
+}
---
+++ test1/src/org/luckystars/scala/tests/book04/ScalaMain.scala
@@ -0,0 +1,10 @@
+package org.luckystars.scala.tests.book04
+
+
+object ScalaMain {
+  
+	def main(args: Array[String]) {
+		println("main")  
+	}
+	
+}
